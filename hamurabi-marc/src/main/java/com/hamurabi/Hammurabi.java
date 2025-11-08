bd2797ff14b68edf5724f15dcd20332054fcb1ce
mmcgough8 <mwm9490@icloud.com>
2025-11-08 00:50:20 -0500

    "added responses for when the user tries to use more resources than they have"

--- hamurabi-marc/src/main/java/com/hamurabi/Hammurabi.java
+++ hamurabi-marc/src/main/java/com/hamurabi/Hammurabi.java
@@ -48,12 +48,16 @@ public class Hammurabi {
         System.out.println( "\nHAIL, MIGHTY HAMMURABI, CHOSEN SHEPHERD OF SUMER!\n" +
                             "Thou art in year " + this.year + " of thy sacred ten-year reign!\n\n" +
                             "By the mercy of the gods, " + this.starved + " among thy people have perished from hunger in the year past.\n\n" +
-                            "Kris and Paul have smiled upon thee! " + this.journeyed + " souls have journeyed from distant lands to dwell beneath thy benevolent rule.\n\n" +
+                            "Kris and Paul have smiled upon thee! " + this.journeyed + 
+                            " souls have journeyed from distant lands to dwell beneath thy benevolent rule.\n\n" +
                             "Thy flock now numbers " + this.flock + " SOULS, entrusted to thy wisdom and care.\n\n" +
-                            "By the grace of Raz, God of Agriculture, thy fields have yielded " + this.harvested + " BUSHELS from the earth, blessed with " + this.grainPerAcre + " bushels per acre!\n\n" +
-                            "Alas! The cursed clankers, spawn of darkness, have devoured " + this.clankersAte + " BUSHELS from thy stores, leaving " + this.storedBushels + " BUSHELS in the Royal Storehouse.\n\n" +
+                            "By the grace of Raz, God of Agriculture, thy fields have yielded " + this.harvested + 
+                            " BUSHELS from the earth, blessed with " + this.grainPerAcre + " bushels per acre!\n\n" +
+                            "Alas! The cursed clankers, spawn of darkness, have devoured " + this.clankersAte + 
+                            " BUSHELS from thy stores, leaving " + this.storedBushels + " BUSHELS in the Royal Storehouse.\n\n" +
                             "Thy dominion encompasses " + this.acresOwned + " ACRES of fertile land.\n\n" +
-                            "The merchants in the marketplace decree that each acre may be bought or sold for " + this.landPrice + " BUSHELS of grain in this season.\n\n" +
+                            "The merchants in the marketplace decree that each acre may be bought or sold for " + this.landPrice + 
+                            " BUSHELS of grain in this season.\n\n" +
                             "---\n\n" +
                             "What is thy command, O Exalted One?");
     }
@@ -79,7 +83,40 @@ public class Hammurabi {
                            "THE GAME HAS ENDED");
     }
 
- 
+    int askHowManyAcresToBuy (int price, int bushels)
+        sanity check - does the player have enough bushels?
+        Response if False - System.out.println("FOLLY, O GREAT HAMMURABI!\n\n" +
+                                               "Dost thou mock thy faithful scribes?!\n\n" +
+                                               "The Royal Storehouse contains a mere " + this.storedBushels + " bushels!\n\n" +
+                                               "Thy decree demands grain that exists only in the realm of dreams!\n\n" +
+                                               "Kris and Paul frown upon such impossible commands!\n\n" +
+                                               "Speak again, O Exalted One, with wisdom befitting thy station!");
 
 
+    int askHowManyAcresToSell(int acresOwned)
+        sanity check - does the player have enough acres to sell?
+        Response if False - System.out.println("IMPOSSIBLE, O GREAT HAMMURABI!\n\n" +
+                                               "Thy dominion encompasses but " + this.acresOwned + " acres of land!\n\n" +
+                                               "Thou canst not plant seeds upon fields that exist not!\n\n" +
+                                               "Raz herself cannot harvest from phantom soil!\n\n" +
+                                               "Speak a wiser decree, O Exalted One!");
+
+
+    int askHowMuchGrainToFeedPeople(int bushels)
+        sanity check - does the player have enough bushels?
+            Response if False - System.out.println("O GREAT HAMMURABI, SURELY THOU DOST JEST!\n\n" +
+                                                   "The Royal Storehouse contains but " + this.storedBushels + " bushels of grain!\n\n" +
+                                                   "Thy command exceeds the bounty bestowed upon thee by Raz!\n\n" +
+                                                   "The scribes implore thee to reconsider thy decree, O Exalted One!");
+
+
+    int askHowManyAcresToPlant(int acresOwned, int population, int bushels)
+        sanity check - does the player have enough acres, grain, and people to do the planting? 
+        Leftover grain goes into storage.
+        Response if False - public void printInsufficientResources(String resourceType, int available, int requested) {
+            System.out.println("INCONCEIVABLE, O GREAT HAMMURABI!\n\n" +
+                               "Thy decree demands " + requested + " " + resourceType + ",\n" +
+                               "yet thou possessest but " + available + "!\n\n" +
+                               "Even the mighty gods cannot conjure resources from the ether!\n\n" +
+                               "Speak again with wisdom, O Exalted One!");
 }
